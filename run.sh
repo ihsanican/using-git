862d67c8aa872b2933818a168ee18987364b938a
ihsanican <ihsanmaulana498@gmail.com>
2024-09-26 17:32:25 +0300

    adding shell script to run for loop inside the ssh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+base_dir="/home/maulanmi/using-git/data"
+output_dir="/home/maulanmi/using-git/ORF_output"
+
+mkdir -p "$output_dir"
+
+for filename in "$base_dir"/*/GCA*; do
+    fna_file=$(find "$filename" -name "*.fna")
+
+    if [[ -f "$fna_file" ]]; then
+        base_name=$(basename "$fna_file" .fna)
+        # Define the output file paths
+        output_file="$output_dir/${base_name}_output.fna"
+
+	python gene_finder.py "$fna_file" > "$output_file"
+	echo "done"
+    fi
+done
