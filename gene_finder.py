7ba9328212dbc29c14e2f598c4a16eb4e971fb79
ihsanican <ihsanmaulana498@gmail.com>
2024-09-26 11:27:21 +0300

    adding a function to read reverse complement and solve six reading frames problem

--- gene_finder.py
+++ gene_finder.py
@@ -1,4 +1,18 @@
 from Bio import SeqIO
+import Bio.Seq as BS
+
+def ReverseComplement(text):
+	complement=""
+	for i in range(len(text)):
+		if text[i]=="A":
+			complement+="T"
+		elif text[i]=="T":
+                        complement+="A"
+		elif text[i]=="G":
+                        complement+="C"
+		else:
+			complement+="G"
+	return complement[::-1]
 
 def gene_finder(file):
 	record = list(SeqIO.parse(file,"fasta"))
@@ -11,26 +25,37 @@ def gene_finder(file):
 	stop_codons = ["TAA","TAG","TGA"]
 	amino_acids = []
 
-	for i in range(3): # run for three reading frames
-		stop = False
-		idx1,idx2 = 0,i
-		while stop==False:
-			for j in range(idx2,N,3):
-				if j>=(N-3):
-					stop=True
-					break
-				if seq[j:j+3]==start_codon:
-					idx1=j
-					break
-			for j in range(idx1+3,N,3):
-				if j>=(N-3):
-					stop=True
-					break
-				if seq[j:j+3] in stop_codons:
-					idx2=j
-					if seq[idx1:idx1+3]==start_codon:
-						amino_acids.append(seq[idx1:idx2+3])
-					break
-			idx2=idx1+3
+	rev = ReverseComplement(genome)
 
-	return amino_acids
+	for seq in [genome,rev]:
+		for i in range(3): # run for three reading frames
+			stop = False
+			idx1,idx2 = 0,i
+			while stop==False:
+				for j in range(idx2,N,3):
+					if j>=(N-3):
+						stop=True
+						break
+					if seq[j:j+3]==start_codon:
+						idx1=j
+						break
+				for j in range(idx1+3,N,3):
+					if j>=(N-3):
+						stop=True
+						break
+					if seq[j:j+3] in stop_codons:
+						idx2=j
+						if seq[idx1:idx1+3]==start_codon:
+							amino_acids.append(seq[idx1:idx2+3])
+						break
+				idx2=idx1+3
+
+	proteins = []
+	for acid in amino_acids:
+		RNA = BS.transcribe(acid)
+		protein = BS.translate(RNA)
+		if protein[:-1] not in proteins:
+			proteins.append(protein[:-1])
+
+	return proteins
+
